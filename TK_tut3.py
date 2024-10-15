92f3d6fbea9c42130e4bf4de9951495c469e7854
AftabAhmad5 <geraltofrivia372@gmail.com>
2024-10-15 20:48:14 +0500

    tutorial 3

--- TK_tut3.py
+++ TK_tut3.py
@@ -1,1 +1,46 @@
-#now let clean up the UI for better responsive ness
+#now let clean up the UI for better responsiveness
+import tkinter as tk
+
+# creating a window name it root
+root = tk.Tk()
+# name the app
+root.title("Form App")
+
+def add_to_list():
+    #getting input data
+    text = entry.get()
+    # checking to avoid blank field entered
+    if text:
+        text_list.insert(tk.END, text)
+        # to make the entry field empty after entering data use delete method
+        text_list.delete(0,tk.END) # 0 mean start from the very first till END of the letter 
+
+
+# creating a frame its a container which has its own grid layout its like a box in a box which can be arranged and what ever data in the second box can be arranged with it.
+
+frame = tk.Frame(root) # here root is the main window inside which we have create a frame
+# place the frame in the top left corner
+frame.grid(row=0, column=0)
+
+# now creating an entry field insde the frame 
+entry = tk.Entry(frame)
+
+# now we are adjusting it inside the frame
+entry.grid(row=0, column=0)
+
+# adding keyboard bind say if someone want to enter data using enter key like not clicking on add button
+entry.bind("<Return>", lambda event: add_to_list()) # with help of lambda function we added an argument to add_to_list function to take this extra key bind feature
+
+# this button will add input text to a list
+entry_btn = tk.Button(frame, text="Add", command=add_to_list)
+entry_btn.grid(row=0, column=1)
+
+# let add a listbox that will hold all the values that we have added
+text_list = tk.Listbox(frame)
+text_list.grid(row=1,column=0)
+
+# now let call a function that will add stuffs to the list once Add button is clicked
+
+
+
+root.mainloop()
